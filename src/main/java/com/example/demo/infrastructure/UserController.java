a666112a0ce40c5412cd12779ee0d8ba0bc48105
camiloghs21 <camiloghs21@gmail.com>
2025-09-28 10:23:26 -0700

    cuadrando los cors del backend

--- src/main/java/com/example/demo/infrastructure/UserController.java
+++ src/main/java/com/example/demo/infrastructure/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -22,7 +21,6 @@ import com.example.demo.infrastructure.Auth.LoginRequest;
 import com.example.demo.infrastructure.Auth.LoginResponse;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api")
 public class UserController {
 
